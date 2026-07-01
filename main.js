9d9e210c9e354ec87c5a4f5305a128fb0d0282b2
GaHub101 <gabetig45@gmail.com>
2026-07-01 06:35:46 +0200

    aktualisiere Betrag

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function calculateNumber() {
   const now = new Date();
 
   const monthsPassed = (now.getFullYear() - start.getFullYear()) * 12 + (now.getMonth() - start.getMonth());
-  const total = Math.max(0, monthsPassed) * 150-192.60-50.20-76.8;
+  const total = Math.max(0, monthsPassed) * 150-192.60-50.20-76.8-61.7;
 
   return total;
 }
